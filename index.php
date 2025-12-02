126c6896dd1a75601fe73c62e848b221c332895d
Pahri <ftyvfyy@gmail.com>
2025-12-02 13:29:47 +0700

    Data Siswa

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Siswa</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rerum, eveniet repellat accusantium reiciendis ad corrupti inventore voluptate necessitatibus delectus architecto in consequuntur beatae itaque est perferendis quidem debitis? Velit?</p>
+    <H1>Data Siswa</H1>
+</body>
+</html>l
